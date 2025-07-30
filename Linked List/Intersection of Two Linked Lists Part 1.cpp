3c99e2787642a22ac43965441aa6ee9ec82f401d
Tirth9978 <tirthppatel9978@gmail.com>
2025-07-30 10:42:04 +0530

    I add mre

---
+++ Linked List/Intersection of Two Linked Lists Part 1.cpp
@@ -0,0 +1,30 @@
+// This is the Problem of the Leetcode 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        map<ListNode * ,int> mp;
+        while(headA != NULL){
+            mp[headA] ++ ;
+            headA = headA -> next;
+        }
+        while(headB != NULL){
+            mp[headB] ++ ;
+            headB = headB -> next;
+        }
+
+        for (auto x : mp){
+            if (x.second > 1){
+                return x.first;
+            }
+        }
+        return NULL;
+    }
+};
