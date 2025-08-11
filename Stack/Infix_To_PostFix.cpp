289cd5633eea6863a907a5e1038ad3e4557829ba
Tirth9978 <tirthppatel9978@gmail.com>
2025-08-11 17:38:28 +0530

    I add infix to postFix problem

---
+++ Stack/Infix_To_PostFix.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<stack>
+using namespace std ;
+
+int priority(char ch){
+     if (ch == '^'){
+          return 3;
+     }
+     else if (ch == '/' || ch == '*'){
+          return 2;
+     }
+     else if (ch == '+' || ch =='-'){
+          return 1;
+     }
+     return -1;
+}
+
+string infoxToPostfix(string str){
+     stack<char> st;
+     string ans ="";
+     for (int i=0;i<str.size();i++){
+          if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9') ) {
+               ans += str[i];
+          }
+          else if (str[i] == '(' ){
+               st.push(str[i]);
+          }
+          else if (str[i] == ')'){
+               while(!st.empty() && st.top() != '('){
+                    ans += st.top();
+                    st.pop();
+               }
+               st.pop();
+          }
+          else {
+               while(!st.empty() && priority(str[i]) <= priority(st.top())) {
+                    ans += st.top();
+                    st.pop();
+               }
+               st.push(str[i]);
+          }
+     }
+     while(!st.empty()){
+          ans += st.top();
+          st.pop();
+     }
+     return ans ;
+}
+
+int main(){
+
+     string str ;
+     cin >> str ;
+     cout << infoxToPostfix(str) << endl;
+     return 0;
+}
