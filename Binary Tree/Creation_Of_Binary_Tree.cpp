a1698298e68427555ad25bde5256551a8bfe37be
Tirth9978 <tirthppatel9978@gmail.com>
2025-08-13 10:37:09 +0530

    I started the Tree Part

---
+++ Binary Tree/Creation_Of_Binary_Tree.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+
+
+// Node Calss 
+class Node{
+     public : 
+          int data ;
+          Node * left ;
+          Node * right ;
+     Node (int data) {
+          this-> data = data ;
+          this->left = NULL ;
+          this->right = NULL ;
+     }
+};   
+
+
+int main(){
+     
+     return 0;
+}
