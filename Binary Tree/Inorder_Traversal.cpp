a139a0b685d51830849e3a33ac1afbd53f4aa22c
Tirth9978 <tirthppatel9978@gmail.com>
2025-08-16 21:20:46 +0530

    I add Inorder

---
+++ Binary Tree/Inorder_Traversal.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<queue>
+using namespace std ;
+
+
+// Node Calss 
+class Node{
+     public : 
+          int data ;
+          Node * left ;
+          Node * right ;
+     Node (int data) {
+          this-> data = data ;
+          this->left = NULL ;
+          this->right = NULL ;
+     }
+};   
+
+Node * builtTree(Node * root){
+     cout << "Enter the Number : " ;
+     int data ;
+     cin >> data ;
+     if (data == -1){
+          return NULL ;
+     }
+
+     root = new Node(data); 
+
+     cout << "Data is Going to Inserting in the Left of " << data << endl;;
+     root -> left = builtTree(root->left);
+
+     cout << "Data is Going to inderting to the right of " << data << endl ;
+     root->right = builtTree(root->right);
+
+     return root ;
+}
+
+void Inorder(Node * head){
+     if (head == NULL){
+          return ;
+     }
+
+     Inorder(head -> left);
+     cout << head -> data << " ";
+     Inorder(head -> right);
+     return ;
+}
+
+int main(){
+     Node * root = NULL ;
+     root = builtTree(root);
+     Inorder(root);
+     return 0;
+}
