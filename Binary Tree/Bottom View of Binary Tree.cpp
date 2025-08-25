e249dfbf6bcb382c45bf80048d14c28d2781fcc9
Tirth9978 <tirthppatel9978@gmail.com>
2025-08-25 15:29:06 +0530

    I have added new onr

---
+++ Binary Tree/Bottom View of Binary Tree.cpp
@@ -0,0 +1,43 @@
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+        vector<int> ans ;
+        if (root == NULL){
+            return ans ;
+        }
+        map<int,int> node ; 
+        queue<pair<Node * , int>> q;
+        
+        q.push({root , 0});
+        
+        while(!q.empty()){
+            pair<Node * , int> p = q.front();
+            Node * frontNode = p.first ;
+            int Hd = p.second ;
+            q.pop();
+            
+            node[Hd] = frontNode->data;
+            
+            if (frontNode -> left != NULL){
+                q.push({frontNode->left , Hd-1});
+            }
+            if (frontNode -> right != NULL){
+                q.push({frontNode->right , Hd +1});
+            }
+        }
+        
+        for (auto x : node){
+            ans.push_back(x.second);
+        }
+        return ans ;
+    }
+};
