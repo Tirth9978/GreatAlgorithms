fe9b3cf4714f803cd57ebe1cd474c3b64a2618ca
Tirth Patel <tirthppatel9978@gmail.com>
2026-07-08 18:14:23 +0530

    Add Disjoin Set implementation in C++
    Implement Disjoin Set data structure with union by rank and path compression.

---
+++ Disjoin set/main.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+class DisjoinSet {
+    vector<int> rank ;
+    vector<int> parent;
+
+public: 
+    DisjoinSet(int n) {
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        for (int i=0;i<=n;i++) {
+            parent[i] = i;
+        }
+    }
+    int findParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ult_u = findParent(u);
+        int ult_v = findParent(v);
+        if (ult_u == ult_v) return ;
+
+        if (rank[ult_u] < rank[ult_v]) {
+            parent[ult_u] = ult_v;
+        }
+        else if (rank[ult_v] < rank[ult_u]){
+            parent[ult_v] = ult_u;
+        }
+        else {
+            parent[ult_v] = ult_u;
+            rank[ult_u] ++ ;
+        }
+        return ;
+    }
+    bool isConnect(int u,int v){
+        if (findParent(u) == findParent(v)) return 1;
+        return 0;
+    }
+};
+
+int main(){
+    DisjoinSet ds(7);
+    ds.unionByRank(1,2);
+    ds.unionByRank(2,3);
+    ds.unionByRank(4,5);
+    if (ds.isConnect(1,5)){
+        cout << "Yes 1 and 5 are connect\n";
+    }else cout << "No 1 and 5 are not connect\n";
+
+    ds.unionByRank(6,7);
+    ds.unionByRank(5,6);
+    ds.unionByRank(3,7);
+    if (ds.isConnect(1,5)){
+        cout << "Yes 1 and 5 are connect\n";
+    }else cout << "No 1 and 5 are not connect\n";
+    return 0;
+}
