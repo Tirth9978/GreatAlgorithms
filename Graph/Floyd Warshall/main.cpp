39b498534980c1d4ef581d9082ee9c4aeb41ca5d
Tirth Patel <tirthppatel9978@gmail.com>
2026-07-08 16:01:03 +0530

    Implement Floyd-Warshall algorithm in main.cpp

---
+++ Graph/Floyd Warshall/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        for (int k=0;k<dist.size();k++){
+            for (int i=0;i<dist.size();i++){
+                for (int j=0;j<dist.size();j++){
+                    if (dist[i][k] < 1e8 && dist[k][j] < 1e8)
+                        dist[i][j] = min(dist[i][k] + dist[k][j] , dist[i][j]) ;
+                }
+            }
+        }
+        return ;
+    }
+};
